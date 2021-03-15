8e8245816d1665f9dde50ad526d4cde7131e8d5e
Dayana Viana <dayanatsv@gmail.com>
2021-03-15 10:14:39 -0500

    Observable|Observer with Data Class

--- RxJava/app/src/main/java/com/android_training/rxjava/MainActivity.kt
+++ RxJava/app/src/main/java/com/android_training/rxjava/MainActivity.kt
@@ -3,6 +3,7 @@ package com.android_training.rxjava
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.android_training.rxjava.models.User
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -15,38 +16,45 @@ import io.reactivex.schedulers.Schedulers
 //Subscriptions: Its a bond between Observable and Observer
 //Disposable: It is used to dispose the bonding between Observable and Observer
 class MainActivity : AppCompatActivity() {
+    lateinit var user: User
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        getObservable().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribeWith(getObserver())
-
-
+        init()
     }
 
-    fun getObservable(): Observable<String>{
-        return Observable.just("data1","data2","data3","data4")
+    private fun init() {
+        user = User(email = "dayana@gmail.com", password = "password")
+
+        getObservable()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(getObserver())
     }
 
-    fun getObserver(): Observer<String>{
-        return object: Observer<String>{
-            override fun onComplete() {
-                Log.d("myAp", "onComplete")
-            }
+    fun getObservable(): Observable<User>{
+        return Observable.just(user)
+    }
 
+    fun getObserver(): Observer<User>{
+        return object: Observer<User>{
             //Setup the bonding between Observable and Observer
             //Disposable: It is used to dispose the bonding between Observable and Observer
             override fun onSubscribe(d: Disposable) {
-                Log.d("myAp", "onSubscribed")
+                Log.d("myApp", "Login Observer Subscribed")
             }
 
             //Receives string one by one
-            override fun onNext(t: String) {
-                Log.d("myAp", "onNext, $t")
+            override fun onNext(u: User) {
+                Log.d("myApp", "User received:, ${u.email}")
+            }
+
+            override fun onComplete() {
+                Log.d("myApp", "Receiving data complete.")
             }
 
             override fun onError(e: Throwable) {
-                Log.d("myAp", "onError")
+                Log.d("myAp", "Error receiving Data. ${e.message}")
             }
 
         }
---
+++ RxJava/app/src/main/java/com/android_training/rxjava/models/User.kt
@@ -0,0 +1,6 @@
+package com.android_training.rxjava.models
+
+class User (
+    var email: String,
+    var password: String
+)
